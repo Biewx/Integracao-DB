b478af13f81aea3aaafc4671396b9c510366ba92
Biel <biel@gmail.com>
2025-04-01 10:16:10 -0300

    Implementação do banco de dados

--- Database/Form1.Designer.cs
+++ Database/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Database
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(341, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 25);
+            label1.TabIndex = 0;
+            label1.Text = "LOGIN";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(106, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(106, 256);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Senha:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(187, 150);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(187, 256);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(341, 363);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Form1";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
--- Database/Form1.cs
+++ Database/Form1.cs
@@ -1,10 +1,61 @@
+using MySql.Data.MySqlClient;
+
 namespace Database
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = textBox1.Text;
+                string senha = textBox2.Text;
+
+                if (usuario != "" && senha != "")
+                {
+                    string conexaoBanco = "Server=localhost; Database=sistemaLogin; Uid=root; pwd=''";
+                    MySqlConnection conexao = new MySqlConnection(conexaoBanco);
+
+                    conexao.Open();
+
+                    string consultaUsuario = "select * from usuarios where usuario = @usuario and senha = @senha ";
+
+                    MySqlCommand comandoSql = new MySqlCommand(consultaUsuario, conexao);
+
+                    comandoSql.Parameters.AddWithValue("@usuario", usuario);
+                    comandoSql.Parameters.AddWithValue("@senha", senha);
+
+                    int registro = Convert.ToInt32(comandoSql.ExecuteScalar());
+
+                    if (registro > 0)
+                    {
+                        MessageBox.Show("Login bem sucedido!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario ou senha não encontrados");
+                    }
+                    conexao.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Preencha os campos.");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao realizar login" + ex.Message);
+            }
         }
     }
 }
